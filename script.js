7d293a2f1178f0d360ffbe0c5ef0cd83c5213799
behrokhGitHub <behrokh79@gmail.com>
2020-01-02 10:36:16 -0800

    solve the sneaky return to zero problem

--- script.js
+++ script.js
@@ -3,25 +3,27 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
+// getting current time using Date class
+var date = new Date();
+console.log(date);
+
+let currHr = date.getHours();
+let currMin = date.getMinutes();
+let currSec = date.getSeconds();
+console.log("Hour: " + currHr + ", Minute: " + currMin + ", Seconds: " + currSec);
+
+let hrPosition = (currHr * 30) + (currMin * 0.5);
+// for each second, minHandle moves (6/60) degree
+let minPosition = (currMin * 6) + (currSec * 6/60);
+let secPosition = (currSec * 6);
+
 function runTheClock () {
-  // getting current time using Date class
-  var date = new Date();
-  console.log(date);
 
-  let currHr = date.getHours();
-  let currMin = date.getMinutes();
-  let currSec = date.getSeconds();
-  console.log("Hour: " + currHr + ", Minute: " + currMin + ", Seconds: " + currSec);
-
-  let hrPosition = 0;
-  let minPosition = (currMin * 6) + ((currSec * 6)/60);
-  let secPosition = (currSec * 6);
-
-  if ( 0 <= currHr <= 12 ) {
-    hrPosition =  (currHr*30) + (currMin * 0.5);
-  } else {
-    hrPosition = ((currHr - 12)* 30) + (currMin * 0.5);
-  }
+  // moving clocks using math, and not relying on Date object
+  // we move the handles, by degree,
+  hrPosition = hrPosition + 30/360;
+  minPosition = minPosition + 6/60;
+  secPosition = secPosition + 6;
 
   // Moving elements using javascript
   HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
