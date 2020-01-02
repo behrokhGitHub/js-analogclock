21088a112b2acc490b64cb8998f6c011b067792c
behrokhGitHub <behrokh79@gmail.com>
2020-01-02 09:39:33 -0800

    showing current time using math

--- script.js
+++ script.js
@@ -3,9 +3,24 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-let hrPosition = 20;
-let minPosition = 130;
-let secPosition = 267;
+// getting current time using Date class
+var date = new Date();
+console.log(date);
+
+let currHr = date.getHours();
+let currMin = date.getMinutes();
+let currSec = date.getSeconds();
+console.log("Hour: " + currHr + ", Minute: " + currMin + ", Seconds: " + currSec);
+
+let hrPosition = 0;
+let minPosition = (currMin * 6);
+let secPosition = (currSec * 6);
+
+if ( 0 <= currHr <= 12 ) {
+  hrPosition =  (currHr*30) + (currMin * 0.5);
+} else {
+  hrPosition = ((currHr - 12)* 30) + (currMin * 0.5);
+}
 
 // Moving elements using javascript
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
