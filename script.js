4d8325cb2efdcd8cf729a20d984e5730b1b83883
behrokhGitHub <behrokh79@gmail.com>
2020-01-01 22:42:54 -0800

    moving elements using javascript

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// Target ids
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+let hrPosition = 20;
+let minPosition = 130;
+let secPosition = 267;
+
+// Moving elements using javascript
+HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
