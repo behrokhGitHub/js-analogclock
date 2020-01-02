b219d22b76bf2d02363534dc8999819fb1f18316
behrokhGitHub <behrokh79@gmail.com>
2020-01-02 09:54:45 -0800

    make the clock move second by second

--- script.js
+++ script.js
@@ -3,26 +3,30 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-// getting current time using Date class
-var date = new Date();
-console.log(date);
+function runTheClock () {
+  // getting current time using Date class
+  var date = new Date();
+  console.log(date);
 
-let currHr = date.getHours();
-let currMin = date.getMinutes();
-let currSec = date.getSeconds();
-console.log("Hour: " + currHr + ", Minute: " + currMin + ", Seconds: " + currSec);
+  let currHr = date.getHours();
+  let currMin = date.getMinutes();
+  let currSec = date.getSeconds();
+  console.log("Hour: " + currHr + ", Minute: " + currMin + ", Seconds: " + currSec);
 
-let hrPosition = 0;
-let minPosition = (currMin * 6);
-let secPosition = (currSec * 6);
+  let hrPosition = 0;
+  let minPosition = (currMin * 6) + ((currSec * 6)/60);
+  let secPosition = (currSec * 6);
 
-if ( 0 <= currHr <= 12 ) {
-  hrPosition =  (currHr*30) + (currMin * 0.5);
-} else {
-  hrPosition = ((currHr - 12)* 30) + (currMin * 0.5);
+  if ( 0 <= currHr <= 12 ) {
+    hrPosition =  (currHr*30) + (currMin * 0.5);
+  } else {
+    hrPosition = ((currHr - 12)* 30) + (currMin * 0.5);
+  }
+
+  // Moving elements using javascript
+  HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+  MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+  SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
 }
 
-// Moving elements using javascript
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+var interval = setInterval (runTheClock, 1000);
